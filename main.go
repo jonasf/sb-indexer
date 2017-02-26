867694efd6c642cb77a5832242ebe2f5511b6df4
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2017-02-26 18:17:34 +0100

    Add possibility to set ElasticSearch url via env variables, add delayed start (to wait for dependant Docker containers)

--- main.go
+++ main.go
@@ -3,20 +3,38 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
 
-	esURLPtr := flag.String("es-url", "http://localhost:9200", "ElasticSearch URL")
+	//Delayed start to wait for dependencies to start
+	delayedStartDuration := os.Getenv("DELAYED-START")
+	if delayedStartDuration != "" {
+		duration, _ := time.ParseDuration(delayedStartDuration)
+		log.Println("Delaying start for ", duration)
+		time.Sleep(duration)
+	}
+
+	esURL := os.Getenv("ES-URL")
+
+	if esURL == "" {
+		esURLPtr := flag.String("es-url", "http://localhost:9200", "ElasticSearch URL")
+		flag.Parse()
+		esURL = *esURLPtr
+	}
+	log.Println("Elasticsearch server address: ", esURL)
+
 	esIndexNamePtr := flag.String("es-index-name", "articles", "ElasticSearch index name")
 	sbAPIURLPtr := flag.String("sb-api-url", "https://www.systembolaget.se/api/assortment/products/xml", "Systembolaget API URL")
 
 	flag.Parse()
 
-	log.Println("es-url:", *esURLPtr)
+	log.Println("es-url:", esURL)
 	log.Println("es-index-name:", *esIndexNamePtr)
 	log.Println("sb-api-url:", *sbAPIURLPtr)
 
-	indexer := NewIndexer(*esURLPtr, *esIndexNamePtr)
+	indexer := NewIndexer(esURL, *esIndexNamePtr)
 	indexer.Index(*sbAPIURLPtr)
 }
